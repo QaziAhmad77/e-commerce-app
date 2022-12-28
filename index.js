fa775ccd51ce114f70d4b0501326bfa50f18a8fc
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2022-12-28 21:35:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+require('dotenv').config();
 // const dotenv = require("dotenv");
 // dotenv.config();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-const router = require("./routes/index");
+const router = require('./routes/index');
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("Database connected Successfully");
+    console.log('Database connected Successfully');
   })
   .catch((err) => {
-    console.log("Some error while connecting to database", err);
+    console.log('Some error while connecting to database', err);
   });
 
-app.use("/api", router);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use((req, res) => {
-  return res.status(404).send("Error 404, Route not found");
+  return res.status(404).send('Error 404, Route not found');
 });
